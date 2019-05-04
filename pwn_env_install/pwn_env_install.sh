b7b6518334393b4ee993773d7dd621b34d921209
ray-cp <raycp@protonmail.com>
2019-05-04 23:40:15 +0800

    pwn env install update

--- pwn_env_install/pwn_env_install.sh
+++ pwn_env_install/pwn_env_install.sh
@@ -4,33 +4,160 @@
 # Description: pwn environment install script for ubuntu
 # Fiel: pwn_env_install.sh
 
+# update source list
+## backup the sources.list
+echo "updating apt source"
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo chmod 666 /etc/apt/sources.list
+## update
+echo deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial main restricted universe multiverse > /etc/apt/sources.list
+echo deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-updates main restricted universe multiverse >> /etc/apt/sources.list
+echo deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-backports main restricted universe multiverse >> /etc/apt/sources.list
+echo deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-security main restricted universe multiverse >> /etc/apt/sources.list
+## restore 
+sudo chmod 660 /etc/apt/sources.list
+## update
+sudo apt-get update -y
+
+# install curl  
+echo "install curl"
+sudo apt install curl -y
+
+# install pip
+echo "install python-pip"
+sudo apt install python-pip -y
+
 #install vim
-sudo apt-get install vim
+echo "install vim"
+sudo apt-get install vim -y
+echo "custom configure vim"
+cp vimrc ~/.vimrc
+
 #install tmux
-sudo apt-get install tmux
+echo "install tmux"
+sudo apt-get install tmux -y
+echo "custom configure tmux"
+cp tmux.conf ~/.tmux.conf
+
+# install git
+echo "install git"
+sudo apt-get install git -y
 
 # install ssh
-sudo apt-get install openssh-server
+echo "install ssh server"
+sudo apt-get install openssh-server -y
 
 # some lib
-sudo apt-get install bison
+echo "install lib:"
+echo "\tbiscon"
+sudo apt-get install bison -y
+echo "\tgawk"
+sudo apt-get install gawk -y
+echo "\tgcc-multilib"
+sudo apt-get install gcc-multilib -y
+echo "\tg++-multilib"
+sudo apt-get install g++-multilib -y
 
-# build debug glibc
-sudo apt-get install 
-cd ~
-mkdir work
-cd work
-for GLIBC_VERSION in '2.19' '2.23' '2.24' '2.27' '2.28'
-do
-	sudo mkdir -p /glibc/$GLIBC_VERSION
-	wget http://mirrors.ustc.edu.cn/gnu/libc/glibc-${GLIBC_VERSION}.tar.gz
-	tar xf glibc-${GLIBC_VERSION}.tar.gz
-	cd glibc-${GLIBC_VERSION}
-	mkdir build
-	cd build
-	../configure --prefix=/glibc/${GLIBC_VERSION}/ --disable-werror --enable-debug=yes
-	sudo make
-	sudo make install
-	cd ../../
-	sudo rm glibc-${GLIBC_VERSION}.tar.gz
-done
+# install patchelf
+echo "install patchelf"
+sudo apt-get install patchelf -y
+
+# install pwntools
+echo "install pwntools"
+pip install pwntools
+
+# make dir ~/work/soft to install soft
+mkdir -p ~/work/soft
+
+# install pwndbg
+echo "install pwndbg"
+if [ ! -d "$HOME/work/soft/pwndbg" ]; then
+  git clone https://github.com/pwndbg/pwndbg.git ~/work/soft/pwndbg
+  (cd ~/work/soft/pwndbg && ./setup.sh) 
+else
+  echo "[*] ~/work/soft/pwndbg exists..."
+fi
+
+# install peda
+echo "install peda"
+if [ ! -d "$HOME/work/soft/peda" ]; then
+  git clone https://github.com/longld/peda.git ~/work/soft/peda
+  echo "#source ~/work/soft/peda/peda.py" >> ~/.gdbinit
+else
+  echo "[*] ~/work/soft/peda exists..."
+fi
+
+# install pwn_debug
+echo "install pwn_debug"
+if [ ! -d "$HOME/work/soft/pwn_debug" ]; then
+  git clone https://github.com/ray-cp/pwn_debug.git ~/work/soft/pwn_debug
+  (cd ~/work/soft/pwn_debug && sudo python setup.py install)
+else
+  echo "[*] ~/work/soft/pwn_debug exists..."
+fi
+
+# install zsh
+echo "install zsh"
+sudo apt-get install zsh -y
+## need to exit manually
+echo "[!] ENTER exit manually!"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+bash_aliases=$(cat ~/.zshrc | grep "~/.bash_aliases")
+if [ -z "$bash_aliases" ];then
+  echo "[*] add ~/.bash_aliases in ~/.zshrc"
+cat <<EOF  >>~/.zshrc
+## add ~/.bash_aliases 
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+EOF
+else
+  echo "[*] ~/.bash_aliases exists in ~/.zshrc"
+fi
+## use dircolors
+echo "[*] add ~/.dircolors in ~/.zshrc"
+dircolors -p > ~/.dircolors
+cat <<EOF  >>~/.zshrc
+## enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+EOF
+## install zsh-autosuggestions
+if [ ! -d "$HOME/.zsh/zsh-autosuggestions" ]; then
+  git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+  echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc 
+else
+  echo "[*] ~/.zsh/zsh-autosuggestions exists..."
+fi
+## install zsh-syntax-highlighting
+if [ ! -d "$HOME/.zsh/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+  echo "source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+else
+  echo "[*] ~/.zsh/zsh-syntax-highlighting exists...."
+fi
+
+sudo apt-get install autojump -y
+if [ `grep -c ". /usr/share/autojump/autojump.sh" ~/.zshrc` -eq '0' ]; then
+
+    echo "autojump has been installed"
+else
+    echo . /usr/share/autojump/autojump.sh >> ~/.zshrc
+fi
+
+
+
+sed -i "s/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"ys\"/g" ~/.zshrc
+# change zsh to default shell
+sudo chsh -s /bin/zsh
+echo "[*] ENJOY!"
+echo "if you wanna install glibc with debug symbols go to ~/work/soft/pwn_debug and execute '/build.sh'"
+/bin/zsh
--- pwn_env_install/readme.md
+++ pwn_env_install/readme.md
@@ -1,1 +1,21 @@
 #pwn_env_install.sh
+
+This script trys to install ctf pwn environments, which runs on ubuntu 16.4
+## Component
+* update source.list (you need to change it according to your country and os version)
+* curl
+* python-pip
+* git
+* bison
+* gawk
+* gcc-multilib
+* g++-multilib
+* vim and custom configuration.
+* tmux and custom configuration.
+* zsh and oh-my-zsh and custom configuration.
+* openssh-server
+* patchefl
+* pwntools
+* pwndbg
+* peda
+* pwn_debug
